903191c324cef627af257c64dd7188fa2f948449
c0a2503081 <c0a2503081@edu.teu.ac.jp>
2026-04-28 14:35:36 +0900

    練習3

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -171,6 +171,9 @@ def main():
                 if beam.rct.colliderect(bomb.rct):
                     bomb = None
                     beam = None
+                    bird.change_img(9,screen)
+                    pg.display.update()
+                    time.sleep(1)
 
         key_lst = pg.key.get_pressed()
         if bird is not None:
